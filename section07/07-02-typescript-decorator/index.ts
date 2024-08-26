bf999f97c67094d7d380b2305bd75f421d18dd25
kimyanghoon <yh.kim@ghostpass.ai>
2024-08-26 19:30:50 +0900

    07

---
+++ section07/07-02-typescript-decorator/index.ts
@@ -0,0 +1,8 @@
+function controller(aaaaaaaaaaa: any) {
+  console.log("==================")
+  console.log(aaaaaaaaaaa)
+  console.log("==================")
+}
+
+@controller
+class CatsController {}
